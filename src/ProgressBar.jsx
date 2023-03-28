9410cacbb501c06621e0ff87ccc8f4b374d146f0
Papsan <papsanlysenko@gmail.com>
2023-03-28 19:12:09 +0200

    added progress bar dynamic color

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,10 +1,15 @@
 import style from './ProgressBar.module.css'
 
 export default function ProgressBar({progress}) {
+
+  let RGBArray1 = [238, 132, 215].map(x => progress * x)
+  let RGBArray2 = [142, 223, 252].map(x => (1 - progress) * x)
+  let RGBArray =  Array.from({length: 3}).map((_, index) => RGBArray1[index] + RGBArray2[index])
+
   return (
     <div
       className={style.progressBar}
-      style={{width: `${100 * progress}%`}}
+      style={{width: `${100 * progress}%`, background: `rgb(${RGBArray})`}}
     />
   )
 }
